cfac6522ea042c6fbb3e27946c9760141874eedb
Matthew Einhorn <moiein2000@gmail.com>
2014-06-05 17:55:20 -0400

    Bump version to 0.1.

--- filers/__init__.py
+++ filers/__init__.py
@@ -5,7 +5,7 @@ manipulate files ensuring that they are not lost in the process.
 
 __all__ = ('FilerException', 'config_name')
 
-__version__ = '0.1-dev'
+__version__ = '0.1'
 
 
 config_name = 'Filers'
